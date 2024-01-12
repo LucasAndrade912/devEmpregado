7211f199c9b78eb636429aa7a2cd051438ae7a66
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-01-12 11:42:00 -0300

    setting fonts

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				title: 'Russo One, sans-serif'
+			}
+		},
+	},
+	plugins: [],
 }
-
