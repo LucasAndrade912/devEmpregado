06333b90a47ed071be2886a6ee3287efede154aa
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-01-12 14:01:16 -0300

    create register page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,102 @@
+import { User, Envelope, LockKey } from '@phosphor-icons/react'
+
 export function App() {
 	return (
-		<>
-			<h1>Hello World</h1>
-		</>
+		<div className="w-full h-screen flex items-center flex-col">
+			<h1 className="font-title mt-32 text-[4rem]">
+				<span className="font-title text-purple-secondary">(dev)</span>Empregado
+			</h1>
+
+			<p className="text-center my-12">
+				Dificuldade em lembrar de todas as vagas que se candidatou?
+				<br /> Cadastre-se e lembresse de todas as suas candidaturas.
+			</p>
+
+			<form className="w-[35.25rem]">
+				<div className="input-field flex flex-col gap-4 mb-7">
+					<label htmlFor="name" className="text-lg">
+						Nome <span className="text-red-500">*</span>
+					</label>
+
+					<div className="input border border-black-border px-3 py-[10px] flex items-center gap-3 rounded">
+						<User size={20} color="#2F2F2F" />
+
+						<input
+							id="name"
+							type="text"
+							placeholder="Digite seu nome completo"
+							className="flex-1 placeholder:text-[#797979] outline-none"
+						/>
+					</div>
+				</div>
+
+				<div className="input-field flex flex-col gap-4 mb-7">
+					<label htmlFor="email" className="text-lg">
+						Email <span className="text-red-500">*</span>
+					</label>
+
+					<div className="input border border-black-border px-3 py-[10px] flex items-center gap-3 rounded">
+						<Envelope size={20} color="#2F2F2F" />
+
+						<input
+							id="email"
+							type="email"
+							placeholder="fula@fulano.com"
+							className="flex-1 placeholder:text-[#797979] outline-none"
+						/>
+					</div>
+				</div>
+
+				<div id="passwords" className="grid grid-cols-2 gap-6">
+					<div className="input-field flex flex-col gap-4">
+						<label htmlFor="password" className="text-lg">
+							Senha <span className="text-red-500">*</span>
+						</label>
+
+						<div className="input border border-black-border px-3 py-[10px] flex items-center gap-3 rounded">
+							<LockKey size={20} color="#2F2F2F" />
+
+							<input
+								id="password"
+								type="password"
+								placeholder="********"
+								className="min-w-0 flex-1 placeholder:text-[#797979] outline-none"
+							/>
+						</div>
+					</div>
+
+					<div className="input-field flex flex-col gap-4">
+						<label htmlFor="confirm-password" className="text-lg">
+							Confirmar senha <span className="text-red-500">*</span>
+						</label>
+
+						<div className="input border border-black-border px-3 py-[10px] flex items-center gap-3 rounded">
+							<LockKey size={20} color="#2F2F2F" />
+
+							<input
+								id="confirm-password"
+								type="password"
+								placeholder="********"
+								className="min-w-0 flex-1 placeholder:text-[#797979] outline-none"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<button
+					type="submit"
+					className="mt-10 w-full flex justify-center items-center bg-purple-primary text-white uppercase py-[14px] rounded-lg hover:bg-purple-hover transition-colors">
+					Cadastrar usuário
+				</button>
+			</form>
+
+			<span className="mt-20">
+				Já possui conta no (dev)Empregado?{' '}
+				<a href="#" className="font-semibold text-purple-primary underline">
+					Clique aqui
+				</a>{' '}
+				para se logar
+			</span>
+		</div>
 	)
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,8 +5,18 @@ export default {
 		extend: {
 			fontFamily: {
 				title: 'Russo One, sans-serif'
+			},
+			colors: {
+				purple: {
+					primary: '#9E24D6',
+					secondary: '#833CA3',
+					hover: '#A244D7'
+				},
+				black: {
+					border: '#1E1E1E'
+				}
 			}
-		},
+		}
 	},
 	plugins: [],
 }
