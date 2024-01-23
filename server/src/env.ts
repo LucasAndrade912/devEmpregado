baa2938aca7cf17453aaebc3eda13b01d4fa2b6c
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-01-23 16:41:45 -0300

    update env schema validator

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
 	MONGODB_DATABASE_URL: z.string().url(),
-	PORT: z.number().optional()
+	PORT: z.string().optional()
 })
 
 export const env = envSchema.parse(process.env)
