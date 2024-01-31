2799e1f87772b697802b0a8651a321d63c45daa6
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-01-31 12:37:35 -0300

    create FormField component

---
+++ web/src/components/FormField.tsx
@@ -0,0 +1,40 @@
+import { forwardRef } from 'react'
+
+import { Input } from './Input'
+
+type Props = {
+	label: string
+	type?: string
+	placeholder?: string
+	className?: string
+	icon?: React.ReactNode
+	error?: string
+}
+
+export const FormField = forwardRef<HTMLDivElement, Props>(
+	({ label, type = 'text', placeholder, className, icon, error, ...rest }, ref) => {
+		return (
+			<div className="flex flex-col gap-4 mb-7" ref={ref}>
+				<label htmlFor={label} className="text-lg">
+					{label} <span className="text-red-500">*</span>
+				</label>
+
+				<Input.Root className={error && 'border-red-500'}>
+					{icon && <Input.Icon>{icon}</Input.Icon>}
+
+					<Input.Field
+						id={label}
+						type={type}
+						placeholder={placeholder}
+						className={className}
+						{...rest}
+					/>
+				</Input.Root>
+
+				{error && (
+					<span className="w-full text-right italic text-sm text-red-500">{error}</span>
+				)}
+			</div>
+		)
+	}
+)
