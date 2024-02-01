269b34bb1d6e4261a3fbaa96e6211eaa62d62c3f
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-02-01 17:17:58 -0300

    add forwardRef in Select component

--- web/src/components/Select/SelectRoot.tsx
+++ web/src/components/Select/SelectRoot.tsx
@@ -1,15 +1,25 @@
+import { forwardRef } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { UseFormRegisterReturn } from 'react-hook-form'
 import * as SelectPrimitive from '@radix-ui/react-select'
 
-type Props = {
+interface Props extends UseFormRegisterReturn {
 	children: React.ReactNode[]
 	defaultValue?: string
-	onSelect?: (selectedItem: string) => void
 }
 
-export function SelectRoot({ children, onSelect, defaultValue }: Props) {
-	return (
-		<SelectPrimitive.Root defaultValue={defaultValue} onValueChange={onSelect}>
-			{children}
-		</SelectPrimitive.Root>
-	)
-}
+export const SelectRoot = forwardRef<HTMLButtonElement, Props>(
+	({ children, onChange, name, defaultValue, ...rest }, ref) => {
+		return (
+			<SelectPrimitive.Root
+				name={name}
+				defaultValue={defaultValue}
+				onValueChange={(value) => onChange({ target: { name, value } })}
+				{...rest}>
+				<Slot ref={ref}>{children[0]}</Slot>
+
+				{children.slice(1)}
+			</SelectPrimitive.Root>
+		)
+	}
+)
--- web/src/components/Select/SelectTrigger.tsx
+++ web/src/components/Select/SelectTrigger.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { CaretDown } from '@phosphor-icons/react'
 import * as SelectPrimitive from '@radix-ui/react-select'
@@ -7,18 +8,20 @@ type Props = {
 	className?: string
 }
 
-export function SelectTrigger({ className, children }: Props) {
-	const defaultStyle =
-		'flex items-center justify-between w-[16.5rem] border border-black-border rounded-sm px-[14px] py-[10px]'
-	const style = twMerge(defaultStyle, className)
+export const SelectTrigger = forwardRef<HTMLButtonElement, Props>(
+	({ className, children, ...rest }, ref) => {
+		const defaultStyle =
+			'flex items-center justify-between w-[16.5rem] border border-black-border rounded-sm px-[14px] py-[10px]'
+		const style = twMerge(defaultStyle, className)
 
-	return (
-		<SelectPrimitive.Trigger className={style}>
-			<div className="flex items-center gap-3">{children}</div>
+		return (
+			<SelectPrimitive.Trigger className={style} ref={ref} {...rest}>
+				<div className="flex items-center gap-3">{children}</div>
 
-			<SelectPrimitive.Icon>
-				<CaretDown size={16} color="#060606" weight="bold" />
-			</SelectPrimitive.Icon>
-		</SelectPrimitive.Trigger>
-	)
-}
+				<SelectPrimitive.Icon>
+					<CaretDown size={16} color="#060606" weight="bold" />
+				</SelectPrimitive.Icon>
+			</SelectPrimitive.Trigger>
+		)
+	}
+)
