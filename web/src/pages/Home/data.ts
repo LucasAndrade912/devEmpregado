12a0e86f92b37eddb59611ee1ee81e8741d20356
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-02-03 16:36:05 -0300

    implement request for get jobs

---
+++ web/src/pages/Home/data.ts
@@ -0,0 +1,42 @@
+import { api } from '@lib/api'
+
+import { FilterJobsFields } from './components/Filters'
+
+type Job = {
+	_id: string,
+	company: string,
+	role: string,
+	modality: string,
+	contract: string,
+	salary: number,
+	status: string,
+	job_url: string
+}
+
+type Response = {
+	jobs: Job[]
+	companies: string[]
+	roles: string[]
+}
+
+export async function getJobs(filters: FilterJobsFields): Promise<Response> {
+	const token = localStorage.getItem('token') || ''
+	const headers = { Authorization: `Bearer ${token}` }
+
+	if (filters.company || filters.contract || filters.modality || filters.role) {
+		const { company, role, modality, contract } = filters
+		const options = Object.assign({},
+			company && {company},
+			role && {role},
+			contract && {contract},
+			modality && {modality}
+		)
+
+		const queryString = '?' + new URLSearchParams(options).toString()
+		const { data } = await api.get<Response>(`/jobs${queryString}`, { headers })
+		return data
+	}
+
+	const { data } = await api.get<Response>('/jobs', { headers })
+	return data
+}
