3917328e212ad896f94126591b63b1220e722cd4
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-02-12 13:18:32 -0300

    configure staleTime for queries

--- web/src/lib/queryClient.ts
+++ web/src/lib/queryClient.ts
@@ -1,3 +1,11 @@
 import { QueryClient } from '@tanstack/react-query'
 
-export const queryClient = new QueryClient()
+const oneDay = 1000 * 60 * 60 * 24
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: oneDay
+		}
+	}
+})
